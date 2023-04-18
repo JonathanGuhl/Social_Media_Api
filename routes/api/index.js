5c1648dcbc90ea0f77e8527b9c926ee88504da5c
JonG <teflonjuan@icloud.com>
2023-04-18 05:44:56 -0500

    set up routes for controllers to api routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,7 +3,9 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes.js');
 const thoughtRoutes = require('./thoughtRoutes.js');
 
+//api/users
 router.use('/users', userRoutes );
+//api/thoughts
 router.use('/thoughts', thoughtRoutes);
 
 model.exports = router;
